0e7f35fd90980eacb4be72097a7a72fd333bd0a0
Mateus Henrique <mhenriqq32@gmail.com>
2021-04-26 16:20:27 -0300

    featuring Dark Mode

--- src/componentes/Header/index.tsx
+++ src/componentes/Header/index.tsx
@@ -2,18 +2,34 @@ import format from 'date-fns/format';
 import ptBR from 'date-fns/locale/pt-BR'
 import Link from 'next/link'
 import styles from './styles.module.scss'
+import { useState } from 'react';
 
-export default function Header() {
+export default function Header(props) {
+  const [darkMode, setDarkMode] = useState(false)
+
   const currentDate = format(new Date(), 'EEEEEE, d MMMM', {
     locale: ptBR
   })
 
+  function toggleMode(event) {
+    props.darkMode.current?.classList.toggle('dark')
+    setDarkMode(!darkMode)
+    console.log(event.target)
+  }
+
   return (
     <header className={styles.headerContainer}>
       <Link href="/">
-        <button><img src="/logo.svg" alt="Poscastr"/></button>
+        <a><img src={!darkMode ? '/logo.svg' : '/logo_light.svg'} alt="Poscastr"/></a>
       </Link>
       <p>O melhor para você ouvir, sempre</p>
+      <span className="toggleMode" onClick={toggleMode}>
+        <img
+          src={darkMode ? 
+          "/sun.svg" 
+          : "/moon.svg"} alt={darkMode ? 'Trocar para modo escuro' : 'Trocar para modo claro'}
+        />
+      </span>
       <span>{currentDate}</span>
     </header>
   );
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,14 +3,16 @@ import '../styles/global.scss'
 import styles from '../styles/app.module.scss'
 import Player from '../componentes/Player'
 import { PlayerContextProvider } from '../contexts/PlayerContext'
-
+import React, { useRef } from 'react'
 export default function MyApp({ Component, pageProps }) {
 
-  return (
+  const darkMode = useRef(null)
+
+  return ( 
     <PlayerContextProvider>
-      <div className={styles.wrapper}>
+      <div className={styles.wrapper} ref={darkMode}>
         <main>
-          <Header />
+          <Header darkMode={darkMode} />
           <Component {...pageProps} />
         </main>
         <Player />
