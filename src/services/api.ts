ed4fc573b248ed71e4caae9a1e191212d8d76879
Mateus Henrique <mhenriqq32@gmail.com>
2021-04-26 16:42:47 -0300

    fixing api route

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: 'http://localhost:3333/'
+  baseURL: 'https://solrachix-json-api.herokuapp.com/v1/mateushenrique-dev/podcastr/'
 })
